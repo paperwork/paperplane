71f3831ede2ea5ea225f6d6a5da6431963f6ce66
Marius <marius@twostairs.com>
2018-03-17 20:46:35 +0100

    Replaced env vars with arguments

--- run-consul-agent.sh
+++ run-consul-agent.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+# usage: run-consul-agent.sh <bind ip> <consul server>
 
 consul agent \
-    -bind=$CONSUL_AGENT_BIND_ADDR \
-    -advertise=$CONSUL_AGENT_BIND_ADDR \
+    -bind=$1 \
+    -advertise=$1 \
     -data-dir=/data \
     -config-dir=/config \
     -log-level=info \
     -rejoin \
-    -retry-join=$CONSUL_SERVER \
+    -retry-join=$2 \
     -retry-max=10 \
     -retry-interval=10s
